74745027eff7fe21923a7e5bd30a5eda8d88aa0f
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2024-02-28 12:26:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,8 +6,8 @@ if os.path.exists("local.env"):
     load_dotenv("local.env")
 
 
-API_ID = int(getenv("API_ID", "25574934")) #optional
-API_HASH = getenv("API_HASH", "") #optional
+API_ID = int(getenv("API_ID", "25667600")) #optional
+API_HASH = getenv("API_HASH", "80dce690dfd8a96f83f20bbe23c28258") #optional
 
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = int(getenv("OWNER_ID"))
