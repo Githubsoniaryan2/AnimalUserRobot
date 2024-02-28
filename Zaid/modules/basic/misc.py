8acc39dcbefea4fd75756dee55a2749bce24d314
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2024-02-28 15:24:37 +0530

    Update misc.py

--- Zaid/modules/basic/misc.py
+++ Zaid/modules/basic/misc.py
@@ -11,20 +11,20 @@ from Zaid.helper.PyroHelpers import ReplyCheck
 from Zaid.modules.help import add_command_help
 from Zaid.modules.bot.inline import get_readable_time
 
-alive_logo = ALIVE_PIC or "https://telegra.ph/file/cc0890d0876bc18c19e05.jpg"
+alive_logo = ALIVE_PIC or "https://te.legra.ph/file/ec19cf227791a167abedc.jpg"
 
 if ALIVE_TEXT:
    txt = ALIVE_TEXT
 else:
     txt = (
-        f"** ✘ zαι∂ υѕєявσт ✘**\n\n"
+        f"** ✘ αиɪмαι υѕєявσт ✘**\n\n"
         f"❏ **νєяѕισи**: `2.1`\n"
         f"├• **υρтιмє**: `{str(datetime.now() - START_TIME).split('.')[0]}`\n"
         f"├• **ρутнσи**: `{python_version()}`\n"
         f"├• **ρуяσgяαм**: `{__version__}`\n"
         f"├• **ѕυρρσят**: [Click](t.me/TheSupportChat)\n"
         f"├• **¢нαииєℓ**: [Click](t.me/TheUpdatesChannel)\n"
-        f"└• **яєρσ**: [Click](https://GitHub.com/itz-zaid/Zaid-Userbot)"        
+        f"└• **яєρσ**: [Click](https://te.legra.ph/file/ec19cf227791a167abedc.jpg)"        
     )
 
 @Client.on_message(
